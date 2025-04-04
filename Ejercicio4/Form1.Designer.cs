383d314457ec4f13f611e8ff64eed85fb25696a6
AlexanderTituaña <bryan.crew11@gmail.com>
2025-04-03 22:39:07 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio4/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Ejercicio4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMatrizEntrada = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtMatrizQ = new System.Windows.Forms.TextBox();
+            this.txtMatrizR = new System.Windows.Forms.TextBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtMatrizEntrada
+            // 
+            this.txtMatrizEntrada.BackColor = System.Drawing.SystemColors.Info;
+            this.txtMatrizEntrada.Location = new System.Drawing.Point(354, 71);
+            this.txtMatrizEntrada.Multiline = true;
+            this.txtMatrizEntrada.Name = "txtMatrizEntrada";
+            this.txtMatrizEntrada.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtMatrizEntrada.Size = new System.Drawing.Size(444, 162);
+            this.txtMatrizEntrada.TabIndex = 0;
+            this.txtMatrizEntrada.Text = " ";
+            this.txtMatrizEntrada.TextChanged += new System.EventHandler(this.txtMatrizEntrada_TextChanged);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnCalcular.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnCalcular.Location = new System.Drawing.Point(832, 95);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(102, 33);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click_1);
+            // 
+            // txtMatrizQ
+            // 
+            this.txtMatrizQ.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtMatrizQ.Location = new System.Drawing.Point(354, 300);
+            this.txtMatrizQ.Multiline = true;
+            this.txtMatrizQ.Name = "txtMatrizQ";
+            this.txtMatrizQ.ReadOnly = true;
+            this.txtMatrizQ.Size = new System.Drawing.Size(186, 174);
+            this.txtMatrizQ.TabIndex = 2;
+            this.txtMatrizQ.TextChanged += new System.EventHandler(this.txtMatrizQ_TextChanged);
+            // 
+            // txtMatrizR
+            // 
+            this.txtMatrizR.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.txtMatrizR.Location = new System.Drawing.Point(620, 300);
+            this.txtMatrizR.Multiline = true;
+            this.txtMatrizR.Name = "txtMatrizR";
+            this.txtMatrizR.ReadOnly = true;
+            this.txtMatrizR.Size = new System.Drawing.Size(178, 174);
+            this.txtMatrizR.TabIndex = 3;
+            this.txtMatrizR.TextChanged += new System.EventHandler(this.txtMatrizR_TextChanged);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnLimpiar.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnLimpiar.Location = new System.Drawing.Point(832, 153);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(102, 34);
+            this.btnLimpiar.TabIndex = 4;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(498, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "DESCOMPOSICION QR";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(402, 262);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "MATRIZ Q";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(670, 262);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "MATRIZ R";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1209, 569);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.txtMatrizR);
+            this.Controls.Add(this.txtMatrizQ);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtMatrizEntrada);
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMatrizEntrada;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtMatrizQ;
+        private System.Windows.Forms.TextBox txtMatrizR;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Ejercicio4/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Ejercicio4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double[,] A = ParseMatrix(txtMatrizEntrada.Text);
+                var (Q, R) = GramSchmidt(A);
+
+                txtMatrizQ.Text = MatrixToString(Q);
+                txtMatrizR.Text = MatrixToString(R);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private static (double[,], double[,]) GramSchmidt(double[,] A)
+        {
+            int m = A.GetLength(0);
+            int n = A.GetLength(1);
+            double[,] Q = new double[m, n];
+            double[,] R = new double[n, n];
+
+            for (int j = 0; j < n; j++)
+            {
+                double[] v = new double[m];
+                for (int i = 0; i < m; i++)
+                    v[i] = A[i, j];
+
+                for (int k = 0; k < j; k++)
+                {
+                    double dot = 0.0;
+                    for (int i = 0; i < m; i++)
+                        dot += Q[i, k] * A[i, j];
+
+                    R[k, j] = dot;
+                    for (int i = 0; i < m; i++)
+                        v[i] -= dot * Q[i, k];
+                }
+
+                double norm = Math.Sqrt(DotProduct(v, v));
+                R[j, j] = norm;
+
+                if (norm == 0)
+                    throw new Exception("La matriz contiene columnas linealmente dependientes.");
+
+                for (int i = 0; i < m; i++)
+                    Q[i, j] = v[i] / norm;
+            }
+
+            return (Q, R);
+        }
+
+        private static double DotProduct(double[] v1, double[] v2)
+        {
+            double sum = 0;
+            for (int i = 0; i < v1.Length; i++)
+                sum += v1[i] * v2[i];
+            return sum;
+        }
+
+        private static double[,] ParseMatrix(string input)
+        {
+            var rows = input.Split(new[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+            int rowCount = rows.Length;
+            int colCount = rows[0].Split(' ').Length;
+            double[,] matrix = new double[rowCount, colCount];
+
+            for (int i = 0; i < rowCount; i++)
+            {
+                var values = rows[i].Split(' ').Select(double.Parse).ToArray();
+                if (values.Length != colCount)
+                    throw new Exception("Todas las filas deben tener el mismo número de columnas.");
+
+                for (int j = 0; j < colCount; j++)
+                    matrix[i, j] = values[j];
+            }
+
+            return matrix;
+        }
+
+        private static string MatrixToString(double[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            string result = "";
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                    result += $"{matrix[i, j]:F4} ";
+                result += "\r\n";
+            }
+
+            return result;
+        }
+
+        private void btnCalcular_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                double[,] A = ParseMatrix(txtMatrizEntrada.Text);
+                var (Q, R) = GramSchmidt(A);
+
+                txtMatrizQ.Text = MatrixToString(Q);
+                txtMatrizR.Text = MatrixToString(R);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void txtMatrizQ_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtMatrizR_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtMatrizEntrada_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtMatrizEntrada.Clear();
+            txtMatrizQ.Clear();
+            txtMatrizR.Clear();
+        }
+    }
+}
